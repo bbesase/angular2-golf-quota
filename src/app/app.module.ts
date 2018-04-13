5b2628f6fd44bb76ab830e6242dfecd8cc14361d
bbesase <bbesase51@gmail.com>
2018-04-13 17:38:17 -0400

    Feature/setup-router
    Added a router to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
@@ -7,6 +8,11 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+
+const appRoutes: Routes = [
+  { path: 'login', component: AppComponent },
+  { path: 'quota', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
@@ -17,7 +23,11 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireStorageModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
     
   ],
   providers: [],
